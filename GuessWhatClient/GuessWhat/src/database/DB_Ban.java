a7cc3ff09fbbafca87999589c5fe2f3dce7598f0
엄호정 <>
2020-08-01 00:30:07 +0900

    Update DB_Ban : add method (getMyNum, getBNum, getAllBanList)

--- GuessWhatClient/GuessWhat/src/database/DB_Ban.java
+++ GuessWhatClient/GuessWhat/src/database/DB_Ban.java
@@ -1,8 +1,44 @@
 package database;
 
 import java.sql.*;
+import java.util.LinkedList;
 
 public class DB_Ban extends DBManager {
+	
+	public synchronized static int getMyNum(String id) throws SQLException {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		int myNum = -1;
+
+		try {
+
+			conn = getConn();
+			stmt = conn.createStatement();
+
+			String s;
+			s = "SELECT PNum FROM Professor WHERE Id = '" + id + "'";
+			rs = stmt.executeQuery(s);
+
+			while(rs.next()) {
+				myNum = rs.getInt("PNum");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return myNum;	
+	}
 
 	public synchronized static void addBan(String Ban_name, int PNum) throws SQLException {
 
@@ -38,6 +74,81 @@ public class DB_Ban extends DBManager {
 		}
 	}
 	
+	public synchronized static int getBNum(String id, String name) throws SQLException {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		int pNum = -1;
+		int bNum = -1;
+
+		try {
+			
+			pNum = getMyNum(id);
+
+			conn = getConn();
+			stmt = conn.createStatement();
+
+			String s;
+			s = "SELECT BNum FROM Ban WHERE PNum = '" + pNum + "' AND Name = '" + name + "'";
+			rs = stmt.executeQuery(s);
+
+			while(rs.next()) {
+				bNum = rs.getInt("BNum");
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return bNum;	
+	}
+	
+	public synchronized static LinkedList<String> getAllBanList(int PNum) throws SQLException {
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		LinkedList<String> banList = new LinkedList<String>();
+
+		try {
+
+			conn = getConn();
+			stmt = conn.createStatement();
+
+			String s;
+			s = "SELECT * FROM Ban WHERE PNum = '" + PNum + "'";
+			rs = stmt.executeQuery(s);
+
+			while(rs.next()) {
+				String myBan = rs.getString("Name");
+				banList.add(myBan);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stmt != null)
+					stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return banList;	
+	}
 	
 	public synchronized static void searchBan(int PNum) {
 
