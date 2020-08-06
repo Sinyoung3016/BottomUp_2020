a1a6fc12bb20bd235f1cc884d4b7e64ef53ee9c3
Sinyoung3016 <tasha0415@naver.com>
2020-08-06 17:00:46 +0900

    Modify ProfessorDataModel

--- GuessWhatClient/GuessWhat/src/model/ProfessorDataModel.java
+++ GuessWhatClient/GuessWhat/src/model/ProfessorDataModel.java
@@ -3,7 +3,7 @@ package model;
 import java.net.Socket;
 import java.util.Arrays;
 
-import exam.Result;
+import exam.StuNumResult;
 import exam.Workbook;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -19,39 +19,42 @@ public class ProfessorDataModel {
 	public static Socket socket = null;
 	public static String ID = null;
 	public static Professor professor;
-	public static BanManager[] banMangerList;
-	
-	// for test
-	public static Ban ban = new Ban(1, "컴퓨터프로그래밍1");
-	public static Ban ban2 = new Ban(2, "컴퓨터프로그래밍2");
-	public static Workbook workbook = new Workbook(new String[]{"workbook", "2"});
-	public static BanManager banManager = new BanManager(1, "1차시", "1111", workbook);
-	public static BanManager banManager1 = new BanManager(1, "1차시", "2222", workbook);
-	
-	public static Result num1 = new Result("1", "MultipleChoice", "1", "문정현", "1");
-	public static Result num2 = new Result("2", "MultipleChoice", "1", "창문정현", "2");
-	
-	public static Result num3 = new Result("1", "ShortAnswer", "2", "문정현", "apple");
-	public static Result num4 = new Result("2", "ShortAnswer", "2", "창문정현", "banana");
-	
-	public static Student stu1 = new Student("workbook", "1","문정현","1:apple");
-	public static Student stu2 = new Student("workbook", "2","창문정현","2:banana");
-	//for test
-	
-	public static ObservableList<HBoxModel> ItemList_MyBanManager = FXCollections.observableArrayList(banManager.getBanManager(1));
-	public static ObservableList<HBoxModel> ItemList_MyClass = FXCollections.observableArrayList(ban.getBan(), ban2.getBan());
-	public static ObservableList<HBoxModel> ItemList_MyWorkBook = FXCollections.observableArrayList(workbook.getWorkbook(1));
-	
-	public static ObservableList<Student> ItemList_Students = FXCollections.observableArrayList(stu1, stu2);
-	public static ObservableList<Result> ItemList_Results1 = FXCollections.observableArrayList(num1, num2); 
-	public static ObservableList<Result> ItemList_Results2 = FXCollections.observableArrayList(num3, num4);
+	public static BanManager [] banManagerList;
 
-	public static void addClass(Ban newBan) {
-		ItemList_MyClass.add(newBan.getBan());
+	// for test
+	public static Ban ban = new Ban(1, 1, "컴퓨터프로그래밍1");
+	public static Ban ban2 = new Ban(1, 2, "컴퓨터프로그래밍2");
+	public static Workbook workbook = new Workbook(new String[] { "1", "1", "workbook", "2" });
+	public static BanManager banManager = new BanManager(1, 1, 1, "1차시", "1111", "workbook");
+	public static BanManager banManager1 = new BanManager(1, 1, 2, "2차시", "2222", "workbook");
+
+	public static StuNumResult num1 = new StuNumResult(1, 1, 1, "workbook", 1, "MultipleChoice", "문정현", "1");
+	public static StuNumResult num2 = new StuNumResult(1, 1, 1, "workbook", 1, "MultipleChoice", "창문정현", "2");
+
+	public static StuNumResult num3 = new StuNumResult(1, 1, 1, "workbook", 2, "ShortAnswer", "문정현", "apple");
+	public static StuNumResult num4 = new StuNumResult(1, 1, 1, "workbook", 2, "ShortAnswer", "창문정현", "banana");
+
+	public static Student stu1 = new Student(1, 1, 1, 1, "문정현", "1:apple");
+	public static Student stu2 = new Student(1, 1, 1, 2, "창문정현", "2:banana");
+	// for test
+
+	public static ObservableList<HBoxModel> ItemList_MyBanManager = FXCollections
+			.observableArrayList(banManager.getBanManager(1));
+	public static ObservableList<HBoxModel> ItemList_MyClass = FXCollections.observableArrayList(ban.getBan(1),
+			ban2.getBan(2));
+	public static ObservableList<HBoxModel> ItemList_MyWorkBook = FXCollections
+			.observableArrayList(workbook.getWorkbook(1));
+
+	public static ObservableList<Student> ItemList_Students = FXCollections.observableArrayList(stu1, stu2);
+	public static ObservableList<StuNumResult> ItemList_Results = FXCollections.observableArrayList(num1, num2);
+	public static ObservableList<StuNumResult> ItemList_Results2 = FXCollections.observableArrayList(num3, num4);
+
+	public static void addClass(int n, Ban newBan) {
+		ItemList_MyClass.add(newBan.getBan(n));
 	}
 
-	public static void removeClass(Ban newBan) {
-		ItemList_MyClass.remove(newBan.getBan());
+	public static void removeClass(int n, Ban newBan) {
+		ItemList_MyClass.remove(newBan.getBan(n));
 	}
 
 	public static void addWorkBook(int n, Workbook newWorkBook) {
@@ -71,3 +74,4 @@ public class ProfessorDataModel {
 	}
 
 }
+
