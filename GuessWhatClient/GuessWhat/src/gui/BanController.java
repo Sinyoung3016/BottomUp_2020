5d9acbede0c7d5d8605ed14f9bc36589bd4733fb
hojeong <>
2020-08-07 20:48:03 +0900

    Update BanController

--- GuessWhatClient/GuessWhat/src/gui/BanController.java
+++ GuessWhatClient/GuessWhat/src/gui/BanController.java
@@ -1,7 +1,13 @@
 package gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -32,17 +38,58 @@ public class BanController extends BaseController implements Initializable {
 	public Ban ban;
 	
 	private String className;
+	
+	private void showBanManagerList(int PNum, int BNum) {
+		ProfessorDataModel.ItemList_MyBanManager.clear();
+		String responseMessage = null;
+		try {
+			String requestMessage = "GetAllBanManager:" + PNum + ":" + BNum;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+			writer.println(requestMessage);
+			writer.flush();
+			responseMessage = reader.readLine();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		String[] responseTokens = responseMessage.split(":");
+		if(responseTokens[0].equals("GetAllBanManager")) {
+			if(! responseTokens[1].equals("Success")) {
+				System.out.println("Fail : GetAllBanManager");
+			}
+			else {
+				System.out.println("Success: GetAllBanManager");
+				int n = 1;
+				for(int i = 2 ; i < responseTokens.length ; i++) {	
+					// [2]BMNum:[3]Name:[4]State:[5]Code:[6]Workbook:[7]Student_size
+					int BMNum = Integer.parseInt(responseTokens[i]);
+					String name = responseTokens[i+1];
+					String state = responseTokens[i+2];
+					String code = responseTokens[i+3];
+					String workbook = responseTokens[i+4];
+					int student_size = Integer.parseInt(responseTokens[i+5]);
+					BanManager newBanManager = new BanManager(PNum, BNum, BMNum, name, state, code, workbook, student_size);
+					ProfessorDataModel.addBanManager(n, newBanManager);					
+					i = i+5;
+					n++;
+				}
+				lv_BanManagerList.setItems(ProfessorDataModel.ItemList_MyBanManager);
+			}
+		}  
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 		this.socket = ProfessorDataModel.socket;
 		this.professor = ProfessorDataModel.professor;
 		this.ban = ProfessorDataModel.ban;
 		
-		this.btn_Main.setText(className);
+		this.showBanManagerList(professor.P_Num(), ban.ban_num());
 		
+		className = this.ban.ban_name();
+		this.btn_Main.setText(className);
 
 	}
 	
