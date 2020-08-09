0a765b26222dacf3eb214949922d0d3856292719
Sinyoung3016 <tasha0415@naver.com>
2020-08-09 18:07:12 +0900

    Modify Alert

--- GuessWhatClient/GuessWhat/src/gui/MainPageController.java
+++ GuessWhatClient/GuessWhat/src/gui/MainPageController.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import exception.MyException;
@@ -28,86 +29,89 @@ import model.ProfessorDataModel;
 import room.Ban;
 import user.Professor;
 
-public class MainPageController implements Initializable{
+public class MainPageController implements Initializable {
 	@FXML
 	private Button btn_WorkBookList, btn_CreateNewClass, btn_MyInfo;
 	@FXML
 	private ListView<HBoxModel> lv_ClassList;
-	
+
 	public Socket socket;
-	public Professor professor; 
+	public Professor professor;
 
 	private void showBanList(int PNum) {
-		
+
 		ProfessorDataModel.ItemList_MyClass.clear();
-		
+
 		String responseMessage = null;
 		try {
 			String requestMessage = "GetAllBan:" + PNum;
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+			PrintWriter writer = new PrintWriter(
+					new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
 			writer.println(requestMessage);
 			writer.flush();
 			responseMessage = reader.readLine();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		String[] responseTokens = responseMessage.split(":");
-		
-		if(responseTokens[0].equals("GetAllBan")) {
-			if(! responseTokens[1].equals("Success")) {
+
+		if (responseTokens[0].equals("GetAllBan")) {
+			if (!responseTokens[1].equals("Success")) {
 				System.out.println("Fail : GetAllBan");
-			}
-			else {
+			} else {
 				int n = 1;
-				for(int i = 2 ; i < responseTokens.length ; i++) {	//[0]GetBan:[1]Success:[2]BNum:[3]Name:[4]BM_Size
-					
+				for (int i = 2; i < responseTokens.length; i++) { // [0]GetBan:[1]Success:[2]BNum:[3]Name:[4]BM_Size
+
 					int pNum = PNum;
 					int BNum = Integer.parseInt(responseTokens[i]);
-					String name = responseTokens[i+1];
-					int bmSize = Integer.parseInt(responseTokens[i+2]);
-					
+					String name = responseTokens[i + 1];
+					int bmSize = Integer.parseInt(responseTokens[i + 2]);
+
 					Ban newBan = new Ban(pNum, BNum, name, bmSize);
-					ProfessorDataModel.addClass(n, newBan);					
+					ProfessorDataModel.addClass(n, newBan);
 					i = i + 2;
 					n++;
 				}
-			lv_ClassList.setItems(ProfessorDataModel.ItemList_MyClass);
-			
+				lv_ClassList.setItems(ProfessorDataModel.ItemList_MyClass);
+
 			}
-		}  
+		}
 	}
+
 	private void createNewClass(int PNum) throws MyException, SQLException {
 		String responseMessage = null;
 		try {
 			String requestMessage = "AddBan:" + PNum;
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+			PrintWriter writer = new PrintWriter(
+					new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
 			writer.println(requestMessage);
 			writer.flush();
 			responseMessage = reader.readLine();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		String[] responseTokens = responseMessage.split(":");
-		if(responseTokens[0].equals("AddBan")) {
-			if(!responseTokens[1].equals("Success")) {
+		if (responseTokens[0].equals("AddBan")) {
+			if (!responseTokens[1].equals("Success")) {
 				System.out.println("Fail : AddBan");
-			}
-			else {
+			} else {
 				this.showBanList(PNum);
 			}
-		} 
+		}
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
 		this.socket = ProfessorDataModel.socket;
 		this.professor = ProfessorDataModel.professor;
 		this.showBanList(professor.P_Num());
-			
+
 	}
 
 	public void btn_WorkBookList_Action() {
@@ -129,6 +133,19 @@ public class MainPageController implements Initializable{
 			this.createNewClass(P_num);
 		} catch (Exception e) {
 			System.out.println("Failed : Create New Class");
+			e.printStackTrace();
+		}
+	}
+
+	public void btn_MyInfo_Action() {
+		try {
+			Stage primaryStage = (Stage) btn_MyInfo.getScene().getWindow();
+			Parent main = FXMLLoader.load(getClass().getResource("/gui/MyInfo.fxml"));
+			Scene scene = new Scene(main);
+			primaryStage.setTitle("GuessWhat/MyInfo");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- GuessWhatClient/GuessWhat/src/gui/MyInfoController.java
+++ GuessWhatClient/GuessWhat/src/gui/MyInfoController.java
@@ -116,64 +116,67 @@ public class MyInfoController implements Initializable {
 		// ModifyProfessor:ID:NewEmail:NewPassWord
 		boolean canRequest = true; // 요청을 보낼 수 있는 상태인가
 
-		TextInputDialog pass = new TextInputDialog();
-		pass.setTitle("Modify MyInfo");
-		pass.setHeaderText("본인 확인을 위해 비밀번호를 입력해주세요.");
-		pass.setContentText("PassWord : ");
-		TextField input = pass.getEditor();
-		if (input.getText().equals(ProfessorDataModel.professor.password())) {
-			
-			if (tf_Email.getLength() == 0) { // email 수정안함
-				if (pf_PassWord.getLength() == 0) { // email, password 모두 수정안함
-					canRequest = false;
-					btn_Close_Action();
-				} else { // passWord만 수정
-					if (!this.checkNewPassword(pf_PassWord.getText(), pf_CheckPW.getText())) {
-						canRequest = false;
-						new Alert(Alert.AlertType.WARNING, "비밀번호가 정확하지 않습니다.", ButtonType.CLOSE).show();
-						return;
-					} else {
-						NewPassword = pf_PassWord.getText();
-					}
-				}
-			} else { // email 수정
-				if (pf_PassWord.getLength() == 0) { // email만 수정
-					Email = tf_Email.getText();
-				} else { // email, password 모두 수정
+		if (pf_PassWord.getLength() != 0) {// password 수정 O
+			if (!this.checkNewPassword(pf_PassWord.getText(), pf_CheckPW.getText())) {
+				canRequest = false;
+				new Alert(Alert.AlertType.WARNING, "비밀번호가 정확하지 않습니다.", ButtonType.CLOSE).show();
+				return;
+			} else {// 비번 맞으면
+				TextInputDialog pass = new TextInputDialog();
+				pass.setTitle("Modify MyInfo");
+				pass.setHeaderText("본인 확인을 위해 비밀번호를 입력해주세요.");
+				pass.setContentText("PassWord : ");
+				pass.show();
+				TextField input = pass.getEditor();
+				if (input.getText().equals(ProfessorDataModel.professor.password())) {
 					NewPassword = pf_PassWord.getText();
-					Email = tf_Email.getText();
-				}
-			}
-
-			if (canRequest) {
-				String responseMessage = null;
-				try {
-					String requestTokens = "ModifyProfessor:" + ProfessorDataModel.ID + ":" + Email + ":" + NewPassword;
-					BufferedReader br = new BufferedReader(
-							new InputStreamReader(this.socket.getInputStream(), StandardCharsets.UTF_8));
-					PrintWriter pw = new PrintWriter(
-							new OutputStreamWriter(this.socket.getOutputStream(), StandardCharsets.UTF_8));
-					pw.println(requestTokens);
-					pw.flush();
-					responseMessage = br.readLine();
-				} catch (Exception e) {
-					System.out.println("Error : " + e.getMessage() + " FROM btn_Update_Action");
-				}
-				String[] responseTokens = responseMessage.split(":");
-				if (responseTokens[0].equals("ModifyProfessor")) {
-					if (!responseTokens[1].equals("Success")) {
-						new Alert(Alert.AlertType.WARNING, responseTokens[1], ButtonType.CLOSE).show();
-					} else {
-						ProfessorDataModel.professor.setEmail(Email);
-
-						new Alert(Alert.AlertType.CONFIRMATION, "개인정보가 수정되었습니다.", ButtonType.CLOSE).show();
+					if (tf_Email.getLength() != 0) { // email 수정
+						Email = tf_Email.getText();
 					}
 				}
 			}
+		} else {// password 수정 X
+			TextInputDialog pass = new TextInputDialog();
+			pass.setTitle("Modify MyInfo");
+			pass.setHeaderText("본인 확인을 위해 비밀번호를 입력해주세요.");
+			pass.setContentText("PassWord : ");
+			pass.show();
+			TextField input = pass.getEditor();
+			if (input.getText().equals(ProfessorDataModel.professor.password())) {
+				if (tf_Email.getLength() != 0) { // email 수정
+					Email = tf_Email.getText();
+				}
+			}
 		}
-		else {
+		
+		if (canRequest) {
+			String responseMessage = null;
+			try {
+				String requestTokens = "ModifyProfessor:" + ProfessorDataModel.ID + ":" + Email + ":" + NewPassword;
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(this.socket.getInputStream(), StandardCharsets.UTF_8));
+				PrintWriter pw = new PrintWriter(
+						new OutputStreamWriter(this.socket.getOutputStream(), StandardCharsets.UTF_8));
+				pw.println(requestTokens);
+				pw.flush();
+				responseMessage = br.readLine();
+			} catch (Exception e) {
+				System.out.println("Error : " + e.getMessage() + " FROM btn_Update_Action");
+			}
+			String[] responseTokens = responseMessage.split(":");
+			if (responseTokens[0].equals("ModifyProfessor")) {
+				if (!responseTokens[1].equals("Success")) {
+					System.out.println(responseTokens[1]);
+					new Alert(Alert.AlertType.WARNING, "MyInfo가 수정에 실패했습니다. 잠시후 다시 시도해주세요.", ButtonType.CLOSE).show();
+				} else {
+					ProfessorDataModel.professor.setEmail(Email);
+					new Alert(Alert.AlertType.CONFIRMATION, "MyInfo가 수정되었습니다.", ButtonType.CLOSE).show();
+				}
+			}
+
+		} else
 			new Alert(AlertType.WARNING, "본인확인에 실패했습니다.");
-		}
+
 	}
 
 	private boolean checkNewPassword(String password, String checkPW) {
