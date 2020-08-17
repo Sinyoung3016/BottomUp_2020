84ef6eb8c44f0ae5838e65c16b64d58c553a1a09
Sinyoung3016 <tasha0415@naver.com>
2020-08-17 09:40:53 +0900

    Arrange Main

--- GuessWhatClient/GuessWhat/src/gui/Main.java
+++ GuessWhatClient/GuessWhat/src/gui/Main.java
@@ -2,7 +2,6 @@ package gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import server.Server;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -10,10 +9,7 @@ import javafx.fxml.FXMLLoader;
 public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			//for test
-			//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/gui/MainPage.fxml"));
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/gui/WorkBookList.fxml"));
-			//BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/gui/Home.fxml"));
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/gui/Home.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setTitle("QuessWhat/Home");
 			primaryStage.setOnCloseRequest(ActionEvent -> BaseController.CloseButtonActione());
